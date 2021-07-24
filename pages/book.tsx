41840dfbbf6f0c07ce35abbb70e8dd5023e446a0
sejaljain123 <sejaljain2043@gmail.com>
2021-07-24 19:04:10 +0530

    added book appointment page

---
+++ pages/book.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookAppointment from '../Components/BookAppointment/BookAppointment';
+
+const book = () => {
+  return (
+    <div>
+      <BookAppointment />
+    </div>
+  );
+};
+
+export default book;
